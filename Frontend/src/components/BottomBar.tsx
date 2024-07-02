17464d9d9fe268ee5abedea652f4eb0ca2fb963a
Caleb-Perez <caleb.perez.tdh@gmail.com>
2024-07-02 12:02:09 -0400

    finished home page

---
+++ Frontend/src/components/BottomBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BottomBar: React.FC = () => {
+	return <div className="bottom-bar"></div>;
+};
+
+export default BottomBar;
---
+++ Frontend/src/components/PopUp.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const PopUp: React.FC = () => {
+	const closePopup: React.MouseEventHandler<HTMLButtonElement> = () => {};
+	return (
+		<div className="pop-up" id="pop-up">
+			<button className="close-btn " id="close-btn" onClick={closePopup}>
+				&times;
+			</button>
+			<h2>Create New</h2>
+			<p>Select the type of file you want:</p>
+			<a href="text.html" className="link-button-pop-up">
+				Text File
+			</a>
+			<a href="text.html" className="link-button-pop-up">
+				Python File
+			</a>
+			<a href="text.html" className="link-button-pop-up">
+				C++ File
+			</a>
+			<a href="text.html" className="link-button-pop-up">
+				Java File
+			</a>
+		</div>
+	);
+};
+
+export default PopUp;
