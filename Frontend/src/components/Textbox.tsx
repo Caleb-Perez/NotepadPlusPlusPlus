4ad553ad619b15d5ad8d1f57e34a91f1cb3692d2
Wildline Lincifort <Wildlinelincifort11234@gmail.com>
2024-07-08 21:53:23 -0400

    Added the line numbers

--- Frontend/src/components/Textbox.tsx
+++ Frontend/src/components/Textbox.tsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 
 const TextBox: React.FC = () => {
+	const textAreaRef = useRef<HTMLTextAreaElement>(null);
+	const lineNumberRef = useRef<HTMLDivElement>(null);
+
+    const updateLineNum = () => {
+		if(lineNumberRef.current && textAreaRef.current) {
+			const textLines = textAreaRef.current.value.split("\n");
+			const displayLines = Math.max(textLines.length, 28);
+			const lineNUmbers = Array.from({length: displayLines}, (_, index) => index +1);
+			lineNumberRef.current.innerHTML = lineNUmbers.join("<br>")
+		}
+	}
+
+	const handleScroll = () => {
+		if(lineNumberRef.current && textAreaRef.current) {
+			lineNumberRef.current.scrollTop = textAreaRef.current.scrollTop;
+		}
+	}
+
+	useEffect(() => {
+		const textArea = textAreaRef.current;
+		if (textArea) {
+		  textArea.addEventListener("scroll", handleScroll);
+		  textArea.addEventListener("input",  updateLineNum);
+	
+		  updateLineNum();
+	
+		  return () => {
+			textArea.removeEventListener("scroll", handleScroll);
+			textArea.removeEventListener("input",  updateLineNum);
+		  };
+		}
+	  }, []);
+
+
 	return (
-		<textarea id="text-editor" placeholder="Type your text here..."></textarea>
+		<div className="text-container">
+			<div className="line-numbers" ref={lineNumberRef}/>
+			<textarea 
+			ref = {textAreaRef}
+			id = "text-editor"
+			className="text-area"
+			placeholder="Type your text here..."
+			onScroll={handleScroll}
+			/>
+		</div>
+		
 	);
 };
 
