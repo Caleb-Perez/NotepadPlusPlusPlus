09880ad40b0e589bcc0af50858569faadac46d5d
Caleb-Perez <caleb.perez.tdh@gmail.com>
2024-07-10 14:09:49 -0400

    Updated textbox styling

--- Frontend/src/components/TitleBar.tsx
+++ Frontend/src/components/TitleBar.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import WindowControls from "./WindowControls";
+import TabsBar from "./TabsBar";
 import "./Styles.css";
 
 const TitleBar: React.FC = () => {
 	return (
 		<div className="title-bar">
-			<div className="editor-tabs">
-				<span className="logo">👽 </span>
-				<div className="tab">
-					<div className="tab-title">New Tab</div>
-					<span className="close-tab">x</span>
-				</div>
-				<div className="tab add-tab">+</div>
-			</div>
+			<TabsBar />
 			<WindowControls />
 		</div>
 	);
