3da0a30f922ec5b7c17395f2de074782a873145d
Kagi123 <spark26@angelo.edu>
2024-07-30 18:05:58 +0900

    helpmenu

--- Frontend/src/menus/HelpMenu.ts
+++ Frontend/src/menus/HelpMenu.ts
@@ -1,33 +1,34 @@
 import { MenuProps } from "../components/Menu";
 
 export const HelpMenu: MenuProps = {
-	title: "Help",
-	options: [
-		{ label: "About", class: "", action: () => {
-			alert(`
-			  Notepad+++
-			  Version: 1.0.0
-			`);
-		  } 
-		},
-		{
-			label: "Documentation",
-			class: "",
-			action: () => {
-				alert(`
-				  Getting Started:
-				  1. Install Notepad+++ on your computer.
-				  2. Open the Notepad+++ application.
-		
-				  Features:
-				  - Syntax Highlighting
-				  - File Management
-				  
-				  FAQ:
-				  Q: How do I open a file?
-				  A: Go to File > Open and select the file you want to open.
-				`);
-			  },
-		},
-	],
+  title: "Help",
+  options: [
+    { label: "About", class: "", action: () => {
+        alert(`
+Notepad+++
+Version: 1.0.0
+        `);
+      }
+    },
+    {
+      label: "Documentation",
+      class: "",
+      action: () => {
+        alert(`
+Getting Started:
+1. Install Notepad+++ on your computer.
+2. Open the Notepad+++ application.
+
+Features:
+- Syntax Highlighting
+- File Management
+
+FAQ:
+Q: How do I open a file?
+A: Go to File > Open and select the file you want to open.
+        `);
+      },
+    },
+  ],
 };
+
