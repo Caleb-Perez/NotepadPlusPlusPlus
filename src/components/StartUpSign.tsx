e64d0739df395a08179c9ff1997df0891abb8394
Zahid Benzan <zahidsoto26@gmail.com>
2024-08-15 16:32:00 -0400

    added js file to initial file menu

--- src/components/StartUpSign.tsx
+++ src/components/StartUpSign.tsx
@@ -97,6 +97,11 @@ const StartUpSign: React.FC = () => {
 						onClick={() => setGoToEditPage(true)}>
 						Java File
 					</button>
+					<button
+						className="link-button-pop-up"
+						onClick={() => setGoToEditPage(true)}>
+						Javascript File
+					</button>
 				</div>
 			</main>
 		</div>
