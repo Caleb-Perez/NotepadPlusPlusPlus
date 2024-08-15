ad33452e8e48595ddd25dce82318e10541abb5b9
Caleb-Perez <caleb.perez.tdh@gmail.com>
2024-08-15 13:31:06 -0400

    Added file menu functionality

--- src/menus/FileMenu.ts
+++ src/menus/FileMenu.ts
@@ -1,19 +1,41 @@
 import { MenuProps } from "../components/Menu";
+import { appWindow } from "@tauri-apps/api/window";
+
 
 export const FileMenu: MenuProps = {
 	title: "File",
 	options: [
-		{ label: "New", class: "", action: () => alert("New clicked") },
+		{ label: "New", class: "", action: () => {const event = new KeyboardEvent('keydown', {
+					  bubbles: true,
+					  cancelable: true,
+					  key: 't',
+					  ctrlKey: true,
+					});
+					document.dispatchEvent(event);} },
 		{
 			label: "Open",
 			class: "menu-file-open",
-			action: () => alert("Open clicked"),
+			action: () => {
+					const event = new KeyboardEvent('keydown', {
+					  bubbles: true,
+					  cancelable: true,
+					  key: 'o',
+					  ctrlKey: true,
+					});
+					document.dispatchEvent(event);
+			},
 		},
 		{
 			label: "Save",
 			class: "menu-file-save",
-			action: () => alert("Save clicked"),
+			action: () => {const event = new KeyboardEvent('keydown', {
+				bubbles: true,
+				cancelable: true,
+				key: 's',
+				ctrlKey: true,
+			  });
+			  document.dispatchEvent(event);},
 		},
-		{ label: "Exit", class: "", action: () => alert("Exit clicked") },
+		{ label: "Exit", class: "", action: () => appWindow.close() },
 	],
 };
